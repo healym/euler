8ccfbf79bf222d869ec0d75378b131fa1de98ca8
mhealy <mhealy@mst.edu>
2017-05-17 17:15:15 -0500

    updated to improve styling and accuracy

--- prob4/prob4.hs
+++ prob4/prob4.hs
@@ -3,7 +3,7 @@ import System.IO
 
 mults = [ x * y | x <- [100..999], y <- [100..999] ]
 
-palindromes = [ x | x <- mults, show(x) == reverse(show(x))]
+palindromes = [ x | x <- mults, show x == (reverse . show) x ]
 
 main = do
-  putStrLn $ head $ reverse $ palindromes
+  putStrLn $ show $ head $ reverse $ sort $ palindromes
