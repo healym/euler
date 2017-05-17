2c8239c552658ad8a1f4a6ef902efa982503a000
Matthew Healy <mhrh3@mst.edu>
2017-05-17 18:55:17 -0500

    finish problem 5

--- prob5/prob5.hs
+++ prob5/prob5.hs
@@ -1,4 +1,10 @@
 import Data.List
 import System.IO
 
+factors n = [ x | x <- [1..n], n `mod` x == 0 ]
 
+primes = [ x | x <- [1..], factors x == [1,x] ]
+
+main = do
+   putStrLn $ show $ product $ 24 : takeWhile ( < 20 ) primes
+  -- 9 and 16 are the largest numbers below 20 with repeated prime factors
