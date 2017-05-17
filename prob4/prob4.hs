bbdb2b56368746f6a745840c00fa35c04fe7ce56
mhealy <mhealy@mst.edu>
2017-05-17 16:56:30 -0500

    prototype recorded

---
+++ prob4/prob4.hs
@@ -0,0 +1,9 @@
+import Data.List
+import System.IO
+
+mults = [ x * y | x <- [100..999], y <- [100..999] ]
+
+palindromes = [ x | x <- mults, show(x) == reverse(show(x))]
+
+main = do
+  putStrLn $ head $ reverse $ palindromes
