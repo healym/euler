873f6cbb87c3aaf50309aedd16feb772929f24a0
Matthew Healy <mhrh3@mst.edu>
2017-08-01 16:16:21 -0500

    finished optimizations

--- prob10/prob10.hs
+++ prob10/prob10.hs
@@ -3,8 +3,8 @@ import System.IO
 
 factors :: Int -> [Int]
 factors n
-    | n == 1 = [1]
-    | otherwise = let x = head([i|i<-[1..n],n `mod` i==0]) in [x] ++ (factors $ n `div` x)
+    | n == 1 = []
+    | otherwise = let x = head([ i | i <- [2..n],n `mod` i==0]) in [x] ++ (factors $ n `div` x)
 
 primes_below :: Int -> [Int]
 primes_below n = [x | x <- [2..n],  (head (factors x)) == x]
