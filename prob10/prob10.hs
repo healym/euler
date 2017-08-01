a69889aea5a7e893970b0fcdd22eab0e6353740a
Matthew Healy <mhrh3@mst.edu>
2017-08-01 15:41:52 -0500

    think i got it now, did some optimizing and fixed some type signatures

--- prob10/prob10.hs
+++ prob10/prob10.hs
@@ -1,9 +1,11 @@
 import Data.List
 import System.IO
 
-factors n = [x | x <- [1..n], n `mod` x == 0]
+factors :: Int -> [Int]
+factors n = [x | x <- [2..n], n `mod` x == 0]
 
-primes_below n = [x | x <- [1..n], factors x == [1,x]]
+primes_below :: Int -> [Int]
+primes_below n = [x | x <- [2..n],  (head (factors x)) == x]
 
 main = do
 putStrLn $ show $ sum $ primes_below 2000000
