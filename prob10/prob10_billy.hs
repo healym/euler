a0e4dfe58f7c902b5a4c99f636a8b47e65f7405e
Matthew Healy <mhrh3@mst.edu>
2017-08-09 18:17:33 -0500

    fixed missing argument

--- prob10/prob10_billy.hs
+++ prob10/prob10_billy.hs
@@ -5,5 +5,5 @@ import System.IO
 primes_below :: Int -> Int -> [Int] -> [Int]
 primes_below acc num primes
     | acc == num = primes
-    | filter(\x -> x `mod` acc /= 0) == [] = primes_below (acc + 1) num (primes ++ [acc])
+    | filter(\x -> (x `mod` acc /= 0)) primes == [] = primes_below (acc + 1) num (primes ++ [acc])
     | otherwise = primes_below (acc + 1) num primes
