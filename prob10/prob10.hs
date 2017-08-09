6fdc06fbf70c4d8a4eae9bc66a3bfbb74c38d8ba
Matthew Healy <mhrh3@mst.edu>
2017-08-09 15:47:30 -0500

    only check for factors up to sqrt n

--- prob10/prob10.hs
+++ prob10/prob10.hs
@@ -4,7 +4,7 @@ import System.IO
 factors :: Int -> [Int]
 factors n
     | n == 1 = []
-    | otherwise = let x = head([ i | i <- 2: [3,5..n],n `mod` i==0]) in [x] ++ (factors $ n `div` x)
+    | otherwise = let x = head([ i | i <- 2: [3,5..sqrt(n)] ++ [n],n `mod` i==0]) in [x] ++ (factors $ n `div` x)
 
 primes_below :: Int -> [Int]
 primes_below n = [x | x <- [2..n],  (head (factors x)) == x]
