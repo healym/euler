81c824d32a466f7c953bcf9e01a9ea03ad35a067
Matthew Healy <mhrh3@mst.edu>
2017-08-10 10:40:56 -0500

    only seeing 2 as prime

--- prob10/prob10_billy.hs
+++ prob10/prob10_billy.hs
@@ -5,5 +5,8 @@ import System.IO
 primes_below :: Int -> Int -> [Int] -> [Int]
 primes_below acc num primes
     | acc == num = primes
-    | filter(\x -> (x `mod` acc /= 0)) primes == [] = primes_below (acc + 1) num (primes ++ [acc])
+    | filter(\x -> (mod x acc /= 0)) primes == [] = primes_below (acc + 1) num (primes ++ [acc])
     | otherwise = primes_below (acc + 1) num primes
+
+main = do
+putStrLn $ show $ sum $ primes_below 2 2000000 []
