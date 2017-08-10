9a934fa1b31d9ed65db79d3650d6d8707c7270a5
Matthew Healy <mhrh3@mst.edu>
2017-08-10 12:14:13 -0500

    returns pair perfectly

--- prob09/naive.hs
+++ prob09/naive.hs
@@ -1,10 +1,9 @@
 import Data.List
 import System.IO
 
-triples = [ (m^2-n^2,2*m*n,m^2 + n^2) | m <- [1..], n <- [1..] ]
+triples = [ (m^2-n^2,2*m*n,m^2 + n^2) | n <- [1..100], m <- [n..100] ]
 
-magic_triple Int -> (Int,Int,Int)
-magic_triple n = [ (a,b,c) | (a,b,c) <- triples, a+b+c == n ]
+magic_triple n = head $ [ (a,b,c) | (a,b,c) <- triples, a+b+c == n ]
 
 main = do
-putStrLn $ show $ magic_triple 1000
+putStrLn $ show $ map product $ magic_triple 1000
