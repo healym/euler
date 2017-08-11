d94e06361d79a315701d1fc123a7a12d7549599b
healym <mhrh3@r11mhrh3.managed.mst.edu>
2017-08-11 10:46:59 -0500

    solution works, still not pretty

--- prob02/node/app/prob2.js
+++ prob02/node/app/prob2.js
@@ -1,18 +1,19 @@
-var fibs = [1,1,2,3,5];
+var fibs = [1,2,3,5];
 
 var current = 5;
-var fib_ind = 4;
+var fib_ind = 3;
 var total = 0;
 
 while(current < 4000000) {
  current += fibs[fib_ind - 1];
  fib_ind++;
- fibs.push(current);
+ if (current < 4000000) fibs.push(current);
 }
 
 for(i = 0; i< fibs.length; i++) {
-  total += fibs[i];
+  if ( fibs[i] % 2 == 0) total += fibs[i];
 }
 
+console.log(fibs);
 console.log(total);
 return total;
