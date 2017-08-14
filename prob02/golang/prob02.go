824881556031909b0872f463d78add1648cbdf9f
healym <mhrh3@r11mhrh3.managed.mst.edu>
2017-08-14 10:06:52 -0500

    added working go solution

---
+++ prob02/golang/prob02.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println(fibsum(4000000));
+}
+
+func fibsum(num int) int {
+  fibs := []int{1,2,3};
+  var sum int;
+
+  for i:= 0; fibs[len(fibs) - 1] + fibs[len(fibs) - 2] < 4000000; i ++ {
+    fibs = append(fibs,fibs[len(fibs) - 1] + fibs[len(fibs) - 2]);
+  }
+
+  for i:= 0; i < len(fibs); i++ {
+    sum += fibs[i];
+  }
+
+  return sum;
+}
